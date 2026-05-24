9dc8d7f41921db045d7d84633b561a025240d074
Alisson G Costa <alisson.costa84200@gmail.com>
2026-05-23 23:34:30 -0300

    feat(pacientes): criando PacienteController e colocando os endpoints basicos

---
+++ src/main/java/com/costa/API_Clinica/pacientes/controller/PacienteController.java
@@ -0,0 +1,41 @@
+package com.costa.API_Clinica.pacientes.controller;
+
+import com.costa.API_Clinica.pacientes.dto.request.PacienteRequestDto;
+import com.costa.API_Clinica.pacientes.dto.request.PacienteRequestNameDto;
+import com.costa.API_Clinica.pacientes.dto.response.PacienteResponseDto;
+import com.costa.API_Clinica.pacientes.service.PacienteService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("/v1/pacientes")
+@RestController
+@RequiredArgsConstructor
+public class PacienteController {
+
+    private final PacienteService pacienteService;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarPacinete(@Valid @RequestBody PacienteRequestDto dto){
+        pacienteService.createPaciente(dto);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<PacienteResponseDto> listarPacientes(){
+        return pacienteService.listarPaciente();
+    }
+
+    @PatchMapping("/{id}/nomes")
+    @ResponseStatus(HttpStatus.OK)
+    public void alterarNomePacinete(@Valid @RequestBody PacienteRequestNameDto dto, @PathVariable UUID id){
+        pacienteService.alteraNome(dto,id);
+    }
+
+}
