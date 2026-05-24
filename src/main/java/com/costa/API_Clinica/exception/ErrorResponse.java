86f85eed3dbb6d7dd53d08fb187c070df0849530
Alisson G Costa <alisson.costa84200@gmail.com>
2026-05-24 20:45:38 -0300

    feat(excpetion): criando o ErrorResponse

---
+++ src/main/java/com/costa/API_Clinica/exception/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.costa.API_Clinica.exception;
+
+public record ErrorResponse(
+        String timeStamp,
+        int status,
+        String error,
+        String message,
+        String path
+) {
+}
