ee0613244ff6f65cc35f916b490d0b07254c945b
Alisson G Costa <alisson.costa84200@gmail.com>
2026-05-25 21:26:03 -0300

    fix(docs): trocando a rota de pastas

---
+++ src/test/java/com/costa/API_Clinica/ApiClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costa.API_Clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
