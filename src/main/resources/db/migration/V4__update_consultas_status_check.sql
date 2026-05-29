cd143fe31399ee200e6acc34b5547d1815e79e3a
Alisson G Costa <alisson.costa84200@gmail.com>
2026-05-28 23:50:35 -0300

    chore(db): atualizar check constraint de status da consulta

---
+++ src/main/resources/db/migration/V4__update_consultas_status_check.sql
@@ -0,0 +1,6 @@
+ALTER TABLE consultas
+    DROP CHECK ck_consultas_status;
+
+ALTER TABLE consultas
+    ADD CONSTRAINT ck_consultas_status
+    CHECK ( consulta_status IN ('AGENDADA', 'CONCLUIDA', 'VENCIDO'));
